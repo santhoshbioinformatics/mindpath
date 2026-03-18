107efd5d8ff8af8747b781265ec3578f0d47e326
Santhoshkumar Rajeshkannan <sandy@Santhoshkumars-MacBook-Air.local>
2026-03-17 19:01:28 -0700

    Import AIModule into ReflectionsModule

--- src/modules/reflections/reflections.module.ts
+++ src/modules/reflections/reflections.module.ts
@@ -2,8 +2,10 @@
 import { Module } from '@nestjs/common';
 import { ReflectionsController } from './reflections.controller';
 import { ReflectionsService } from './reflections.service';
+import { AIModule } from '../../ai/ai.module';
 
 @Module({
+  imports: [AIModule],
   controllers: [ReflectionsController],
   providers: [ReflectionsService],
 })
