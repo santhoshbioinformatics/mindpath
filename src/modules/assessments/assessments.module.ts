44404c37af7fb9e438c70b54f30e1d39f7eb7145
Santhoshkumar Rajeshkannan <sandy@Santhoshkumars-MacBook-Air.local>
2026-03-17 18:54:40 -0700

    Import NotificationsModule into AssessmentsModule

--- src/modules/assessments/assessments.module.ts
+++ src/modules/assessments/assessments.module.ts
@@ -2,8 +2,10 @@
 import { Module } from '@nestjs/common';
 import { AssessmentsController } from './assessments.controller';
 import { AssessmentsService } from './assessments.service';
+import { NotificationsModule } from '../../notifications/notifications.module';
 
 @Module({
+  imports: [NotificationsModule],
   controllers: [AssessmentsController],
   providers: [AssessmentsService],
   exports: [AssessmentsService],
