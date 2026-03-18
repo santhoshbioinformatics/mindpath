1ba8b2cdb7efdb720fd4ef8d0ea154827917c7ca
Santhoshkumar Rajeshkannan <sandy@Santhoshkumars-MacBook-Air.local>
2026-03-17 18:51:33 -0700

    Import AIModule into InsightsModule

--- src/modules/insights/insights.module.ts
+++ src/modules/insights/insights.module.ts
@@ -3,8 +3,10 @@ import { Module } from '@nestjs/common';
 import { InsightsController } from './insights.controller';
 import { InsightsService } from './insights.service';
 import { CorrelationDetectionJob } from '../../jobs/correlation-detection.job';
+import { AIModule } from '../../ai/ai.module';
 
 @Module({
+  imports: [AIModule],
   controllers: [InsightsController],
   providers: [InsightsService, CorrelationDetectionJob],
   exports: [InsightsService],
