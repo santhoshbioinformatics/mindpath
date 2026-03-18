0873b665acf652785909118d42271b6f55ae2e99
Santhoshkumar Rajeshkannan <sandy@Santhoshkumars-MacBook-Air.local>
2026-03-17 19:05:04 -0700

    Import AIModule into SafetyModule

--- src/modules/safety/safety.module.ts
+++ src/modules/safety/safety.module.ts
@@ -2,8 +2,10 @@
 import { Module } from '@nestjs/common';
 import { SafetyController } from './safety.controller';
 import { SafetyService } from './safety.service';
+import { AIModule } from '../../ai/ai.module';
 
 @Module({
+  imports: [AIModule],
   controllers: [SafetyController],
   providers: [SafetyService],
 })
